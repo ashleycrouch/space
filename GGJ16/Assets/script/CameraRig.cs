1a8ae5b583a652474916595cf879077a16c36593
Tanner Grehawick <ggrehawi@gmu.edu>
2016-01-29 19:47:27 -0500

    started work on camera and rooms

---
+++ GGJ16/Assets/script/CameraRig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraRig : MonoBehaviour {
+
+	public string targetTag = "Player";
+
+	Transform target;
+
+	void Start() {
+		UpdateTarget();
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void UpdateTarget() {
+		target = GameObject.FindGameObjectWithTag(targetTag).transform;
+	}
+
+
+}
---
+++ GGJ16/Assets/script/Room.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+	public int width = 1, height = 1;
+
+	void OnDrawGizmos() {
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.color = Color.white;
+		Vector3 size = Vector3.right * width + Vector3.up * height;
+		Gizmos.DrawWireCube(size / 2, size);
+	}
+	
+}
