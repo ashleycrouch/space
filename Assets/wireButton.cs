1f9235b30c02c7769158d4c69aa64e7484b1f1b8
Ashley <ashley.crouch@rocketmail.com>
2016-01-29 21:30:12 -0500

    made button script

---
+++ Assets/wireButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wireButton : MonoBehaviour
+{
+    public GameObject door;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void onGUI()
+    {
+        //if button is pressed, activate door trigger
+        door.GetComponent<BoxCollider2D>().enabled = false;
+        //else deactivate door trigger
+        door.GetComponent<BoxCollider2D>().enabled = true;
+    }
+}
