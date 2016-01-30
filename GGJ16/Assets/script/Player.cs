601c637ec29cc39a3f488f5f6d8bea9184507581
Matt Kelly <Veloxiac@users.noreply.github.com>
2016-01-29 21:40:02 -0500

    Create Player.cs

---
+++ GGJ16/Assets/script/Player.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float jumpForce;
+    public float speed;
+    public float boostSpeed;
+    public float boostDistance;
+    public float boostCooldown;
+    public LayerMask groundLayers;
+
+    private bool boosting = false;
+    public bool grounded;
+    private bool jumpPressed;
+    private bool jumpReleased;
+    public  bool doubleJumped;
+    private bool facingRight = true;
+    private float boostTimer = 0f;
+    private Vector2 boostTo;
+    private Rigidbody2D my_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        my_Rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+    void Update() {
+        if (Input.GetAxisRaw("Boost") > 0 && boostTimer <= 0) {
+            boosting = true;
+            boostTimer = boostCooldown;
+            boostTo = new Vector2(facingRight ? transform.position.x + boostDistance : transform.position.x - boostDistance, transform.position.y);
+        }
+        if (jumpReleased && Input.GetAxisRaw("Vertical") > 0.50f) {
+            jumpPressed = true;
+            jumpReleased = false;
+        }
+        if(Input.GetAxisRaw("Vertical") == 0) {
+            jumpReleased = true;
+        }
+
+        boostTimer -= Time.deltaTime;
+    }
+	void FixedUpdate () {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        
+
+        if (boosting) {
+            Boost();
+        } else {
+            Move(xInput, yInput);
+        }
+        GroundCheck();
+    }
+
+    private void Move(float x, float y) {
+        my_Rigidbody.velocity = new Vector2(speed*x, my_Rigidbody.velocity.y);
+        if(my_Rigidbody.velocity.x > 0) {
+            transform.localScale = new Vector2(1f,1.5f);
+            facingRight = true;
+        } else if(my_Rigidbody.velocity.x < 0){
+            transform.localScale = new Vector2(-1f, 1.5f);
+            facingRight = false;
+        }
+        if (jumpPressed) {
+            if (grounded || !doubleJumped) {
+                my_Rigidbody.AddForce(new Vector2(0f, jumpForce));
+            }
+            if (!grounded) {
+                doubleJumped = true;
+            }
+            jumpPressed = false;
+        }
+    }
+
+    private void Boost() {
+        my_Rigidbody.velocity = new Vector2(facingRight? boostSpeed : -boostSpeed, 0f);
+        if((facingRight && transform.position.x > boostTo.x) ||
+            (!facingRight && transform.position.x < boostTo.x)) {
+            boosting = false;
+        }
+    }
+
+    private void GroundCheck() {
+        Vector2 bottomLeft = new Vector2(transform.position.x - 0.50f, transform.position.y - 0.8f);
+        Vector2 bottomRight = new Vector2(transform.position.x + 0.50f, transform.position.y - 0.8f);
+        Debug.DrawLine(bottomLeft, bottomRight);
+        if (Physics2D.Linecast(bottomLeft, bottomRight, groundLayers)) {
+            grounded = true;
+            doubleJumped = false;
+        } else {
+            grounded = false;
+        }
+    }
+}
