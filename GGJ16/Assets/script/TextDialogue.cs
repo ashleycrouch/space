65b174bdd67b9ca035406f5fa3933f026f28d0dd
Veloxiac <matthewkelly95@gmail.com>
2016-01-29 23:58:14 -0500

    Text Dialogue

---
+++ GGJ16/Assets/script/TextDialogue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextDialogue : MonoBehaviour {
+
+    public TextAsset textAsset;
+    private char[] chars;
+    public float timeToNextLetter = 0.1f;
+    public float waitForNextLine = 3f;
+
+    private float timer = 0f;
+    private int counter;
+    private int max;
+
+	// Use this for initialization
+	void Awake () {
+        chars = textAsset.ToString().ToCharArray();
+        max = chars.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (counter != max && timer > timeToNextLetter) {
+
+            if (chars[counter].Equals('\n') || counter == max-1) {
+                nextPage();
+                return;
+            }
+            counter++;
+
+            timer = 0f;
+            this.GetComponent<Text>().text += chars[counter];
+            
+        }
+	}
+
+    void nextPage() {
+        if (timer >= waitForNextLine) {
+            this.GetComponent<Text>().text = "";
+            counter++;
+        }
+    }
+}
