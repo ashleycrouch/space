4375207cf5b454db6878ad3e82b2290d631c2694
Ashley <ashley.crouch@rocketmail.com>
2016-01-29 23:30:39 -0500

    updating stuff

--- Assets/wireButton.cs
+++ Assets/wireButton.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class wireButton : MonoBehaviour
 {
     public GameObject door;
+    public bool pressed;
 
     // Use this for initialization
     void Start()
@@ -11,11 +12,17 @@ public class wireButton : MonoBehaviour
 
     }
 
-    void onGUI()
+    public void onClick()
     {
-        //if button is pressed, activate door trigger
         door.GetComponent<BoxCollider2D>().enabled = false;
-        //else deactivate door trigger
-        door.GetComponent<BoxCollider2D>().enabled = true;
+        pressed = true;
     }
+
+    //method that makes the button disconnect the wires when it's not clicked
+    public void disconnect()
+    {
+        door.GetComponent<BoxCollider2D>().enabled = true;
+        pressed = false;
+    }
+
 }
---
+++ Assets/wireManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wireManager : MonoBehaviour
+{
+    public wireButton current;
+    public wireButton[] group;
+    
+    // Use this for initialization
+    void Start()
+    {
+        group = GetComponentsInChildren<wireButton>();
+        current = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(GetComponentInChildren<wireButton>().pressed)
+        //{
+        //    current = 
+        //}
+
+    }
+}
