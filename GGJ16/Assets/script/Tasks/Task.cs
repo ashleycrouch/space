d37d9186db30d951983ec75fde6ab76bc59bf9fc
Veloxiac <matthewkelly95@gmail.com>
2016-01-30 12:56:20 -0500

    Task Parent Class

---
+++ GGJ16/Assets/script/Tasks/Task.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task : MonoBehaviour {
+
+    protected bool completed;
+
+	public virtual void Complete() {
+        completed = true;
+    }
+    public bool isComplete() {
+        return completed;
+    }
+    public virtual Vector2 Location() {
+        return transform.position;
+    }
+}
