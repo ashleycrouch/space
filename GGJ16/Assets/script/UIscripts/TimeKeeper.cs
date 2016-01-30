bccd7b5997c80684e532b09759e85bc1b930f2cf
Veloxiac <matthewkelly95@gmail.com>
2016-01-30 11:07:04 -0500

    Timer

---
+++ GGJ16/Assets/script/UIscripts/TimeKeeper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeKeeper : MonoBehaviour {
+
+    public float timeMultiplier;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer = Time.time*timeMultiplier;
+        int minutes = (int)(timer / 60);
+        int seconds = (int)timer%60;
+        GetComponent<Text>().text = minutes.ToString().PadLeft(2,'0') + ":" + seconds.ToString().PadLeft(2,'0');
+	}
+}
