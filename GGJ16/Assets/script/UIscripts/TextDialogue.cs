89ec0886358d3b7b4149c6eba6a5fa531cec7f22
Veloxiac <matthewkelly95@gmail.com>
2016-01-31 04:58:08 -0500

    soundfordialogue

--- GGJ16/Assets/script/UIscripts/TextDialogue.cs
+++ GGJ16/Assets/script/UIscripts/TextDialogue.cs
@@ -4,38 +4,43 @@ using System.Collections;
 
 public class TextDialogue : MonoBehaviour {
 
-    public TextAsset textAsset;
+    private TextAsset textAsset;
     private char[] chars;
     public float timeToNextLetter = 0.1f;
     public float waitForNextLine = 3f;
+    public AudioSource audioSource;
+    public AudioClip typeSound;
 
     private float timer = 0f;
     private int counter;
     private int max;
+    private bool lastLetter = false;
 
-	// Use this for initialization
-	//void Awake () {
- //       chars = textAsset.ToString().ToCharArray();
- //       max = chars.Length;
-	//}
-	
-	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
 
         if (counter != max && timer > timeToNextLetter) {
 
             if (chars[counter].Equals('\n') || counter == max-1) {
+                if (counter == max - 1 && !lastLetter) {
+                    this.GetComponent<Text>().text += chars[counter];
+                    lastLetter = true;
+                }
                 nextPage();
                 return;
             }
-            counter++;
+            
 
             timer = 0f;
             this.GetComponent<Text>().text += chars[counter];
-            
+            counter++;
+            audioSource.pitch = Random.Range(1f, 1.5f);
+            audioSource.Play();
+
+        } else {
+            audioSource.Stop();
         }
-	}
+    }
 
     void nextPage() {
         if (timer >= waitForNextLine) {
@@ -50,5 +55,6 @@ public class TextDialogue : MonoBehaviour {
         chars = textAsset.ToString().ToCharArray();
         max = chars.Length;
         counter = 0;
+        lastLetter = false;
     }
 }
