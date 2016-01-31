1ac4b3b1957eabcd7d8093b31eac48ae695e42c8
tgrehawi <ggrehawi@gmu.edu>
2016-01-31 03:41:25 -0500

    teleporter manager

---
+++ GGJ16/Assets/script/TeleporterManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class TeleporterManager : MonoBehaviour {
+
+	[Header("Transporters")]
+	public Transporter hallway;
+	public Transporter messHall;
+	public Transporter aiBay;
+	public Transporter cargoBay;
+	public Transporter engineRoom;
+	public Transporter medBay;
+
+	public void hallwayToCargo() {
+		ChangeTransporterDestination(hallway, cargoBay, "Cargo Bay");
+	}
+
+	public void hallwayToMess() {
+		ChangeTransporterDestination(hallway, messHall, "Mess Hall");
+	}
+
+	public void hallwayToAI() {
+		ChangeTransporterDestination(hallway, aiBay, "AI Core");
+	}
+
+	public void messToHallway() {
+		ChangeTransporterDestination(messHall, hallway, "Hallway");
+	}
+
+	public void messToMedbay() {
+		ChangeTransporterDestination(messHall, medBay, "Medbay");
+	}
+
+	public void cargoToHallway() {
+		ChangeTransporterDestination(cargoBay, hallway, "Hallway");
+	}
+
+	public void cargoToEngine() {
+		ChangeTransporterDestination(cargoBay, engineRoom, "Engine Room");
+	}
+
+	public void engineToCargo() {
+		ChangeTransporterDestination(engineRoom, cargoBay, "Cargo Bay");
+	}
+
+	void ChangeTransporterDestination(Transporter a, Transporter b, string roomName) {
+		a.ChangeDestination(b.transform, "transport to " + roomName);
+	}
+
+}
