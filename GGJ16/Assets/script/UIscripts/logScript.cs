1d51a6a72b613e6dd12f96043a630af0254f3e73
Ashley <ashley.crouch@rocketmail.com>
2016-01-30 21:58:13 -0500

    captain's log 5: I made the captain's log

---
+++ GGJ16/Assets/script/UIscripts/logScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class logScript : MonoBehaviour {
+
+    public TextAsset log;
+    private Text display;
+
+	// Use this for initialization
+	void Awake () {
+        Time.timeScale = 0f;
+        display = gameObject.GetComponentInChildren<Text>();
+
+        display.text = log.ToString();
+	}
+	
+    void onClick()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
