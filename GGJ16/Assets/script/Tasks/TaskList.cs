5352dc0a3d7414c8acac7954715438a7244affef
Veloxiac <matthewkelly95@gmail.com>
2016-01-31 01:33:51 -0500

    tasklist done

---
+++ GGJ16/Assets/script/Tasks/TaskList.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskList : MonoBehaviour {
+
+    public Task[] tasks;
+    public int initialTasks;
+
+    private int counter = 0;
+    private TaskManager taskManager;
+
+	void Start () {
+        taskManager = GameObject.Find("TaskManager").GetComponent<TaskManager>();
+	    for(int i = 0; i < initialTasks; i++) {
+            taskManager.AddTask(tasks[i]);
+            counter++;
+        }
+	}
+    public void Next() {
+        if(counter == tasks.Length) {
+            return;
+        }
+        taskManager.AddTask(tasks[counter]);
+        counter++;
+    }
+
+}
