83789e962f882a9040be6183cc29d653046b0737
Tanner Grehawick <ggrehawi@gmu.edu>
2016-01-30 22:52:11 -0500

    made teleporters purdy®

--- GGJ16/Assets/script/Transporter.cs
+++ GGJ16/Assets/script/Transporter.cs
@@ -4,6 +4,7 @@ public class Transporter : MonoBehaviour {
 
 	public Transform destination;
 	public bool snapCamera = false;
+	public GameObject effect;
 
 	void OnInteract() {
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -11,6 +12,13 @@ public class Transporter : MonoBehaviour {
 		destinationPos.z = player.transform.position.z;
 		player.transform.position = destinationPos;
 		Room.SetCurrentFromPlayer();
+		destinationPos.z = effect.transform.position.z;
+		Vector3 pos = transform.position;
+		pos.z = destinationPos.z;
+		if (effect) {
+			Instantiate(effect, pos, Quaternion.identity);
+			Instantiate(effect, destinationPos, Quaternion.identity);
+		}
 		if (snapCamera) {
 			CameraRig.main.Snap();
 		}
